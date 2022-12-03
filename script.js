9975e6b0866de14f43d8fd9c31c9f619284428d9
Unilca <113147796+Unilca@users.noreply.github.com>
2022-12-03 06:18:33 +0300

    create full moving and game rules

--- script.js
+++ script.js
@@ -95,6 +95,23 @@ function createBerry() {
 }
 createBerry();
 
+//direction of move
+//default
+let direction = "right";
+
+//score count
+let input = document.createElement("input");
+document.body.appendChild(input);
+input.style.cssText = `
+margin: auto;
+margin-top: 40px;
+font-size: 20px;
+display: block;
+`;
+
+let score = 0;
+input.value = `your score: ${score}`;
+
 //teach the snake how to move
 //save array with coordinates
 function move() {
@@ -108,25 +125,115 @@ function move() {
   snakeBody[snakeBody.length - 1].classList.remove("snakeBody");
   snakeBody.pop();
 
+  //--------------------------------------------------------------(right default)
   //teach snake ignore walls (//delete class from old cells)
   //if <10 then move right on one cells
-  if (snakeCoordinates[0] < 10) {
-    snakeBody.unshift(
-      document.querySelector(
-        '[posX = "' +
-          (+snakeCoordinates[0] + 1) +
-          '"][posY = "' +
-          snakeCoordinates[1] +
-          '"]'
-      )
+  if (direction == "right") {
+    if (snakeCoordinates[0] < 10) {
+      snakeBody.unshift(
+        document.querySelector(
+          '[posX = "' +
+            (+snakeCoordinates[0] + 1) +
+            '"][posY = "' +
+            snakeCoordinates[1] +
+            '"]'
+        )
+      );
+      //if =10 then start move from left side
+    } else {
+      snakeBody.unshift(
+        document.querySelector(
+          '[posX = "1"][posY = "' + snakeCoordinates[1] + '"]'
+        )
+      );
+    }
+  }
+  //--------------------------------------------------------------(left)
+  else if (direction == "left") {
+    if (snakeCoordinates[0] > 1) {
+      snakeBody.unshift(
+        document.querySelector(
+          '[posX = "' +
+            (+snakeCoordinates[0] - 1) +
+            '"][posY = "' +
+            snakeCoordinates[1] +
+            '"]'
+        )
+      );
+      //if =10 then start move from other side
+    } else {
+      snakeBody.unshift(
+        document.querySelector(
+          '[posX = "10"][posY = "' + snakeCoordinates[1] + '"]'
+        )
+      );
+    }
+  }
+  //--------------------------------------------------------------(up)
+  else if (direction == "up") {
+    if (snakeCoordinates[1] < 10) {
+      snakeBody.unshift(
+        document.querySelector(
+          '[posX = "' +
+            snakeCoordinates[0] +
+            '"][posY = "' +
+            (+snakeCoordinates[1] + 1) +
+            '"]'
+        )
+      );
+      //if =10 then start move from other side
+    } else {
+      snakeBody.unshift(
+        document.querySelector(
+          '[posX = "' + snakeCoordinates[0] + '"][posY = "1"]'
+        )
+      );
+    }
+  }
+  //--------------------------------------------------------------(down)
+  else if (direction == "down") {
+    if (snakeCoordinates[1] > 1) {
+      snakeBody.unshift(
+        document.querySelector(
+          '[posX = "' +
+            snakeCoordinates[0] +
+            '"][posY = "' +
+            (snakeCoordinates[1] - 1) +
+            '"]'
+        )
+      );
+      //if =10 then start move from other side
+    } else {
+      snakeBody.unshift(
+        document.querySelector(
+          '[posX = "' + snakeCoordinates[0] + '"][posY = "10"]'
+        )
+      );
+    }
+  }
+
+  //eat berry
+  if (
+    snakeBody[0].getAttribute("posX") == berry.getAttribute("posX") &&
+    snakeBody[0].getAttribute("posY") == berry.getAttribute("posY")
+  ) {
+    berry.classList.remove("berry");
+    let a = snakeBody[snakeBody.length - 1].getAttribute("posX");
+    let b = snakeBody[snakeBody.length - 1].getAttribute("posY");
+    snakeBody.push(
+      document.querySelector('[posX = "' + a + '"][posY = "' + b + '"]')
     );
-    //if =10 then start move from left side
-  } else {
-    snakeBody.unshift(
-      document.querySelector(
-        '[posX = "1"][posY = "' + snakeCoordinates[1] + '"]'
-      )
-    );
+    createBerry();
+    score++;
+    input.value = `your score: ${score}`;
+  }
+  //Game over rules
+  if (snakeBody[0].classList.contains("snakeBody")) {
+    setTimeout(() => {
+      alert(`Game over (score: ${score})`);
+    }, 200);
+
+    clearInterval(interval);
   }
 
   //give head and body class new cells
@@ -134,6 +241,31 @@ function move() {
   for (let i = 0; i < snakeBody.length; i++) {
     snakeBody[i].classList.add("snakeBody");
   }
+  steps = true;
 }
 
+//move speed
 let interval = setInterval(move, 300);
+
+//steps
+let steps = false;
+
+//EventListener for arrow + Timeout
+
+window.addEventListener("keydown", function (e) {
+  if (steps == true) {
+    if (e.key == "ArrowLeft" && direction != "right") {
+      direction = "left";
+      steps == false;
+    } else if (e.key == "ArrowUp" && direction != "down") {
+      direction = "up";
+      steps == false;
+    } else if (e.key == "ArrowRight" && direction != "left") {
+      direction = "right";
+      steps == false;
+    } else if (e.key == "ArrowDown" && direction != "up") {
+      direction = "down";
+      steps == false;
+    }
+  }
+});
